e1e5cf1121a2b81801c8c1cbebd5acf916ca51d5
elicia <e0420909@u.nus.edu>
2024-08-16 01:05:20 +0100

    embed watson assistant on website, fixed python chatbot

--- python-chatbot/chatbot (old).py
+++ python-chatbot/chatbot (old).py
@@ -1,6 +1,5 @@
 import streamlit as st
 import time
-# from helper import *
 
 from langchain.vectorstores.pgvector import PGVector
 import pandas as pd
@@ -17,8 +16,6 @@ from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenP
 from ibm_watson_machine_learning.foundation_models.utils.enums import DecodingMethods
 from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
 
-
-### HELPER FUNCTIONS #################
 
 # Set up Connections to database and Watsonx AI for LLM Models
 CONNECTION_STRING = "postgresql://postgres:123456@localhost:5433/wateraid"
@@ -95,9 +92,14 @@ def DB_retrieve_and_query(query):
     d = {'response': res,
          'links': links}
     return d
-#######################################
+
 
 st.title("I'm Walter from WaterAid!")
+
+### HELPER FUNCTIONS #################
+
+
+#######################################
 
 # Initialise chat history
 if "messages" not in st.session_state:
@@ -120,73 +122,89 @@ questions = [
 ]
 
 st.markdown("Welcome, I’m Walter from WaterAid. Help me answer a few questions and I can help you find activities that you may enjoy while contributing to our cause!")
-st.markdown("Do note that I am a chatbot and not a real person, and will not be able to offer any services or answers outside of the designed chat!")
 st.divider()
 
 # Build Q&A dialogue conversation & Accept User input
+def chat():
+    prompt = st.chat_input("Type something...")
 
-prompt = st.chat_input("Type something...")
+    if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Get correct question index - If user input asks to run recommendation again
+        if st.session_state.messages[-2]["content"] != "Whereabouts are you based in? Which location would you prefer? \n Hint: I am based in _______. Fill in the blanks.":
+            if prompt == "Again" or prompt == "again":
+                st.session_state.question_index = 0
+                st.session_state.query = ""
 
+        if st.session_state.question_index < len(questions):
+            if st.session_state.question_index == 1:
+                temp = "I like to " + prompt + " in my free time. "
+                st.session_state.query += temp
+
+            Walter_res = questions[st.session_state.question_index]
+            st.session_state.messages.append({"role": "Walter", "content": Walter_res})
+            st.session_state.question_index += 1
+
+        else:
+            temp = "I am based in " + prompt + ". What activities are recommended for me based on the context provided - give the specific name of the activity? Give a reason why each activity is recommended for me."
+            st.session_state.query += temp
+            if st.session_state.messages[-2]["content"] != "Whereabouts are you based in? Which location would you prefer? \n Hint: I am based in _______. Fill in the blanks.":
+                if prompt != "Again" or prompt != "again":
+                    bye_msg = "Thank you for using our recommendation service! Type 'Again' anytime to restart the service"
+                    st.session_state.messages.append({"role": "Walter", "content": bye_msg})
+
+            else: # No more questions, provide recommendations
+                print(st.session_state.query)
+
+                summary = "Based on your responses, here are the top 3 activities recommended for you: \n" 
+                st.session_state.messages.append({"role": "Walter", "content": summary})
+
+                # with st.spinner('Please wait while results load'):
+                #     time.sleep(5)
+
+                retrieve = DB_retrieve_and_query(st.session_state.query)
+                response = retrieve['response']
+                urls = retrieve['links']
+                print(response)
+                print(urls)
+
+                res = response + "\n \n" + urls
+                st.session_state.messages.append({"role": "Walter", "content": res})
+                
+                feedback = "Are you satisfied with our recommendations? If not, type 'Again' to run the recommendation service again!"
+                st.session_state.messages.append({"role": "Walter", "content": feedback})
+            
+
+chat()
+
+# Display chat messages from history on app rerun
 for message in st.session_state.messages:
+    if message['content'] == "Are you satisfied with our recommendations? If not, type 'Again' to run the recommendation service again!":
+        time.sleep(3)
+
+    # if message['content'] == "Based on your responses, here are the top 3 activities recommended for you: \n":
+    #     time.sleep(3)
     with st.chat_message(message["role"]):
+        if message['content'] == "Based on your responses, here are the top 3 activities recommended for you: \n":
+            with st.spinner('Loading... Please wait.'):
+                    time.sleep(8)
         st.markdown(message["content"])
 
-if prompt:
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+        # if message['content'] == "Based on your responses, here are the top 3 activities recommended for you: \n":
+        #     with st.spinner('Loading... Please wait.'):
+        #             time.sleep(8)
+            
+        #     retrieve = DB_retrieve_and_query(st.session_state.query)
+        #     response = retrieve['response']
+        #     urls = retrieve['links']
+        #     print(response)
+        #     print(urls)
 
-    # Get correct question index - If user input asks to run recommendation again
-    if st.session_state.messages[-2]["content"] != "Whereabouts are you based in? Which location would you prefer? \n Hint: I am based in _______. Fill in the blanks.":
-        if prompt == "Again" or prompt == "again":
-            st.session_state.question_index = 0
-            st.session_state.query = ""
-
-    if st.session_state.question_index < len(questions):
-        if st.session_state.question_index == 1:
-            temp = "I like to " + prompt + " in my free time. "
-            st.session_state.query += temp
-
-        Walter_res = questions[st.session_state.question_index]
-        st.session_state.messages.append({"role": "Walter", "content": Walter_res})
-        st.session_state.question_index += 1
-        with st.chat_message("Walter"):
-            st.markdown(Walter_res)
-
-    else:
-        temp = "I am based in " + prompt + ". What activities are recommended for me based on the activities in the context provided - give the specific name of the activity? Give a reason why each activity is recommended for me."
-        st.session_state.query += temp
-        if st.session_state.messages[-2]["content"] != "Whereabouts are you based in? Which location would you prefer? \n Hint: I am based in _______. Fill in the blanks.":
-            if prompt != "Again" or prompt != "again":
-                bye_msg = "Thank you for using our recommendation service! Type 'Again' anytime to restart the service"
-                st.session_state.messages.append({"role": "Walter", "content": bye_msg})
-                with st.chat_message("Walter"):
-                    st.markdown(bye_msg)
-
-        else: # No more questions, provide recommendations
-            print(st.session_state.query)
-
-            summary = "Based on your responses, here are the top 3 activities recommended for you: \n" 
-            st.session_state.messages.append({"role": "Walter", "content": summary})
-            with st.chat_message("Walter"):
-                st.markdown(summary)
-
-                with st.spinner('Please wait while results load'):
-                    time.sleep(5)
-
-                    retrieve = DB_retrieve_and_query(st.session_state.query)
-                    response = retrieve['response']
-                    urls = retrieve['links']
-                    print(response)
-                    print(urls)
-
-                res = response + "\n \n Links for above activities and other potential activities: \n \n" + urls
-                st.session_state.messages.append({"role": "Walter", "content": res})
-                st.markdown(res)
+        #     res = response + "\n \n" + urls
+        #     # st.markdown(message["content"])
+        #     st.session_state.messages.append({"role": "Walter", "content": res})
                 
-            feedback = "Are you satisfied with our recommendations? If not, type 'Again' to run the recommendation service again!"
-            st.session_state.messages.append({"role": "Walter", "content": feedback})
-            time.sleep(2)
-            with st.chat_message("Walter"):
-                st.markdown(feedback)
+        #     feedback = "Are you satisfied with our recommendations? If not, type 'Again' to run the recommendation service again!"
+        #     st.session_state.messages.append({"role": "Walter", "content": feedback})
+        
 
